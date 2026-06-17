7a70576d01a7a60018cc6eaa4ae8d44203631608
okavsar9-boop <okavsar9@gmail.com>
2026-06-17 14:45:06 +0200

    cleared everything to try jpa

--- src/main/java/com/example/quickstart/QuickstartApplication.java
+++ src/main/java/com/example/quickstart/QuickstartApplication.java
@@ -1,7 +1,9 @@
 package com.example.quickstart;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuickstartApplication {
@@ -9,5 +11,14 @@ public class QuickstartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuickstartApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner  commandLineRunner (){
+		return runner -> {
+			System.out.println("Hello World");
+		};
+
+	}
+
 
 }
